e626c3aa22fe9b6e62bc2289682f6f65f9fb2390
mstsurnalyakter <surnalyms@gmail.com>
2024-01-27 11:10:23 +0600

    task1 added

---
+++ module17/task1.js
@@ -0,0 +1,9 @@
+const burgerPrice = 300;
+
+if (burgerPrice >= 500) {
+    console.log(`You get free coke.`);
+}else{
+    console.log(`You get not free coke.`);
+}
+
+burgerPrice >= 500 ? console.log(`You get free coke`): console.log(`You get not free coke`)
