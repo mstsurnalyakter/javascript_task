174ef8fa8ba5a15f54d6a92ecd28a2c0fd20b6c2
mstsurnalyakter <surnalyms@gmail.com>
2024-01-28 19:12:03 +0600

    subtask 1 added

---
+++ loop-tasks/for/odd_even.js
@@ -0,0 +1,16 @@
+/***
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+ */
+
+for (let i = 61; i <= 100; i = i + 2) {
+    console.log(`Odd Number ${i}`);
+}
+
+/***
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
