e7f4b33bf0d92f9d69ac3e7b4649d8cf1dba360d
mstsurnalyakter <surnalyms@gmail.com>
2024-02-05 12:53:59 +0600

    added task 5

--- problem_part_2/task5.js
+++ problem_part_2/task5.js
@@ -1,42 +1,31 @@
-/**&
- * Task-4:
-You are given an array of phone objects, each containing information about the model, brand, and price. Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
-
-Input
-
-
-    const phones = [
-        { model: "PhoneA", brand: "Iphone", price: 95000 },
-        { model: "PhoneB", brand: "Samsung", price: 40000 },
-        { model: "PhoneC", brand: "Oppo", price: 26000 },
-        { model: "PhoneD", brand: "Nokia", price: 35000 },
-        { model: "PhoneE", brand: "Iphone", price: 105000 },
-        { model: "PhoneF", brand: "HTC", price: 48000 },
-    ];
-
+/**
+ * Task -5: (Hard)
+For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
+ const employees = [
+            { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+            { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+            { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+            { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+        ];
  */
 
-const findAveragePhonePrice  = phones =>{
-    let sum = 0;
-    if (Array.isArray(phones)) {
-        for (const phone of phones) {
-            sum += phone.price;
-        }
+const calculateMonthlySalary = (employees) => {
+  let totalMonthlySalary = 0;
+  if (Array.isArray(employees)) {
+    for (const employee of employees) {
+      totalMonthlySalary +=
+        employee.starting + (employee.experience > 0 ? employee.experience : 1 ) * employee.increment;
     }
+  }
 
-    return sum / phones.length;
-}
+  return totalMonthlySalary;
+};
 
+const employees = [
+  { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+  { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+  { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+  { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
 
-
-    const phones = [
-      { model: "PhoneA", brand: "Iphone", price: 95000 },
-      { model: "PhoneB", brand: "Samsung", price: 40000 },
-      { model: "PhoneC", brand: "Oppo", price: 26000 },
-      { model: "PhoneD", brand: "Nokia", price: 35000 },
-      { model: "PhoneE", brand: "Iphone", price: 105000 },
-      { model: "PhoneF", brand: "HTC", price: 48000 },
-    ];
-
-
-console.log(findAveragePhonePrice(phones));
+console.log(calculateMonthlySalary(employees));
